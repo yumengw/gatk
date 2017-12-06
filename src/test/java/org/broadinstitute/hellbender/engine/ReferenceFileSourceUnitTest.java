eae078506c7e80a61ffb0ea96d8dda7a2c528274
JP Martin <jpmartin@google.com>
2017-12-06 12:34:54 -0800

    Test GetSequenceDictionary from jimfs

--- src/test/java/org/broadinstitute/hellbender/engine/ReferenceFileSourceUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/engine/ReferenceFileSourceUnitTest.java
@@ -2,17 +2,25 @@ package org.broadinstitute.hellbender.engine;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
+import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.reference.ReferenceSequenceFileFactory;
+import htsjdk.samtools.util.IOUtil;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.GATKBaseTest;
 
+import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class ReferenceFileSourceUnitTest extends GATKBaseTest {
+
+    private static final Path TEST_REFERENCE = IOUtils.getPath(hg19MiniReference);
 
     @Test(expectedExceptions = UserException.MissingReferenceFaiFile.class)
     public void testReferenceInPathWithoutFai() throws IOException {
@@ -22,7 +30,7 @@ public class ReferenceFileSourceUnitTest extends GATKBaseTest {
             final Path dictPath = jimfs.getPath("reference.dict");
             Files.createFile(dictPath);
 
-            new ReferenceFileSource(refPath);
+            new ReferenceFileSource(refPath).close();
         }
     }
 
@@ -34,7 +42,7 @@ public class ReferenceFileSourceUnitTest extends GATKBaseTest {
             final Path faiFile = jimfs.getPath("reference.fasta.fai");
             Files.createFile(faiFile);
 
-            new ReferenceFileSource(refPath);
+            new ReferenceFileSource(refPath).close();
         }
     }
 
@@ -48,7 +56,26 @@ public class ReferenceFileSourceUnitTest extends GATKBaseTest {
             final Path dictPath = jimfs.getPath("reference.dict");
             Files.createFile(dictPath);
 
-            new ReferenceFileSource(refPath);
+            new ReferenceFileSource(refPath).close();
+        }
+    }
+
+    @Test
+    public void testGetSequenceDictionary() throws IOException {
+        try (FileSystem jimfs = Jimfs.newFileSystem(Configuration.unix())) {
+            final Path refPath = jimfs.getPath("reference.fasta");
+            final Path faiPath = jimfs.getPath("reference.fasta.fai");
+            final Path dictPath = jimfs.getPath("reference.dict");
+            Files.copy(TEST_REFERENCE, refPath);
+            Files.copy(IOUtil.addExtension(TEST_REFERENCE, ".fai"), faiPath);
+            Files.copy(ReferenceSequenceFileFactory.getDefaultDictionaryForReferenceSequence(TEST_REFERENCE),dictPath);
+            try (ReferenceDataSource refDataSource = new ReferenceFileSource(refPath)) {
+                SAMSequenceDictionary sequenceDictionary = refDataSource.getSequenceDictionary();
+                Assert.assertEquals(sequenceDictionary.size(), 4, "Wrong number of sequences in sequence dictionary returned from refDataSource.getSequenceDictionary()");
+                for ( String contig : Arrays.asList("1", "2", "3", "4") ) {
+                    Assert.assertNotNull(sequenceDictionary.getSequence(contig), "Sequence dictionary returned from refDataSource.getSequenceDictionary() lacks expected contig " + contig);
+                }
+            }
         }
     }
 
